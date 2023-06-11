50b6a222e4c58768c701132a46e0c9a8564b27cb
Dominick Dickerson <dominickator122102@gmail.com>
2023-06-11 15:43:38 -0500

    Modified app.js

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,13 +1,60 @@
 import "./App.css";
+import React, {useEffect, useState} from "react";
+import ConfettiExplosion, {ConfettiProps} from 'react-confetti-explosion';
 
 function App() {
 
+  const [guess, setGuess] = useState("");
+  let [randVar, setRandVar] = useState(0);
+  const [isExploding, setIsExploding] = useState(false);
+
+  useEffect(() => {
+    setRandVar(Math.floor(Math.random() * 10));
+  }, []);
+
   const getRandomEmojis = () => {
-    var rand = 0;
-    switch(rand){
+    switch(randVar){
       case 0:
-        return (<h3>🚗💨⏰👴🏻🦕</h3>);
+        return (<h3>🚗💨⏰👴🏻🕺🏻</h3>);
         break;
+      case 1:
+        return (<h3>🚢💔🌊🎶👨‍🦲</h3>);
+        break;
+      case 2:
+        return (<h3>👨‍👩‍👦🍝🔫🇮🇹💼</h3>);
+        break;
+      case 3:
+        return (<h3>🏃‍♂️🍫🚌🪙🎸</h3>);
+        break;
+      case 4:
+        return (<h3>🦁👑🐗🌅🎵</h3>);
+        break;
+      case 5:
+        return (<h3>🦈🌊🛥️👨‍✈️🔫</h3>);
+        break;
+      case 6:
+        return (<h3>🚲🌌👽👦🏻🌟</h3>);
+        break;
+      case 7:
+        return (<h3>🕰️👦🏻🦖👨‍🦳🏎️</h3>);
+        break;
+      case 8:
+        return (<h3>🕶️💊🔫💻🌐</h3>);
+        break;
+      case 9:
+        return (<h3>🦖🦕🚙💥🌴</h3>);
+        break;
+    }
+  };
+
+  const checkGuess = () => {
+    if(randVar === 0){
+      if(guess.toLowerCase() === "back to the future"){
+        return (isExploding && <ConfettiExplosion force={0.8} duration={3000} particleCount={250} width={1600}/>);
+      }
+      else{
+        alert("Incorrect guess! Try again!");
+      }
     }
   };
 
@@ -24,9 +71,11 @@ function App() {
       <div class="emojiContainer">
         <div class="emojis">{getRandomEmojis()}</div>
       </div>
-      <input class="input" placeholder="Please enter guess"></input>
-      <button class="button">Submit</button>
-      <button class="button">Get another movie!</button>
+      <div width="3vw" margin="auto">
+        <input class="input" onChange={(Event) => {setGuess(Event.target.value)}} placeholder="Please enter guess"></input>
+        <button class="button" onClick={() => checkGuess()}>Submit</button>
+        <button class="button" onClick={() => setRandVar(Math.floor(Math.random() * 10))}>Get another movie!</button>
+      </div>
     </>
   );
 }
